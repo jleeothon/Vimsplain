ff739b8a2f121381d7687b6d515574bbd9b61b28
Johnny Lee Othon <jleeothon@gmail.com>
2017-02-28 23:53:30 +0100

    Added a test file

---
+++ test/vimsplaintest.py
@@ -0,0 +1,27 @@
+import pytest
+from subprocess import PIPE
+from subprocess import run
+
+
+LONG_EXAMPLE = """qa\trecord typed characters into named register {0-9a-zA-Z"} (uppercase to append)
+3j\tcursor 3 lines downward
+Y\tyank 1 lines [into buffer x]; synonym for "yy"
+p\tput the text [from register x] after the cursor 1 times
+J\tJoin 2 lines; default is 2
+D\tdelete the characters under the cursor until the end of the line and 0 more lines [into buffer x]; synonym for "d$"
+q\t(while recording) stops recording
+2@a\texecute the contents of register {a-z} 2 times
+ZZ\tstore current file if modified, and exit"""
+
+@pytest.mark.parametrize(
+    "input_,raw_expected",
+    [
+        ("a", "a\tappend text after the cursor 1 times"),
+        ("qa3jYpJDq2@aZZ", LONG_EXAMPLE),
+    ]
+)
+def test_examples(input_, raw_expected):
+    command = "python vimsplain.py {}".format(input_)
+    expected_output = raw_expected + "\n"
+    actual_output = str(run(["python", "vimsplain.py", input_], stdout=PIPE).stdout, 'utf-8')
+    assert actual_output == expected_output
